4ada0fe1233febf0d728f5cbb01ae2e90ab52b6a
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-01 05:43:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash{"color" => pink, "food" => "mac and cheese", "beverage" => "IPA"}
   
 end
 
