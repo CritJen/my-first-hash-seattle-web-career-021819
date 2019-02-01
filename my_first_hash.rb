6d8f56e899287eb8dc922b5aac1bdc7e5c3fda54
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-01 05:51:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,11 @@
 def my_hash
-  your_hash{"color" => "pink", "food" => "mac and cheese", "beverage" => "IPA"}
+  your_hash = {"color" => "pink", "food" => "mac and cheese", "beverage" => "IPA"}
 
 end
 
 
 def shipping_manifest
-  the_manifest{"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
